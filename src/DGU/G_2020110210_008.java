8e8af57ba9a45697a202f3bfd56d020e784cc1be
Kong-E <kng001016@dgu.ac.kr>
2023-10-04 10:19:33 +0900

    feat: 008 수정

--- src/DGU/G_2020110210_008.java
+++ src/DGU/G_2020110210_008.java
@@ -1,61 +1,46 @@
 package DGU;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
 import java.awt.*;
 
+// 예제 9-5 GridLayoutEx : GridLayout으로 입력 폼 만들기
 public class G_2020110210_008 extends JFrame {
     public G_2020110210_008() {
-        setTitle("BoxLayout Sample");
+        setTitle("GridLayout Sample");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout()); //Frame의 기본 content pane의 레이아웃을 FlowLayout으로 설정
 
         JPanel gridPanel = new JPanel();
-        GridLayout grid = new GridLayout(6, 2); // 7 rows (including the row for the textarea), 2 columns
+        GridLayout grid = new GridLayout(6, 2);
         grid.setVgap(5);
         gridPanel.setLayout(grid);
-        
-        // Set padding using EmptyBorder
-        gridPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        gridPanel.setPreferredSize(new Dimension(300, 200));
-
+        gridPanel.setPreferredSize(new Dimension(280, 170));
         gridPanel.add(new JLabel(" 학교"));
-        gridPanel.add(new JTextField("동국대학교"));
-
-        gridPanel.add(new JLabel(" 이름"));
-        gridPanel.add(new JTextField("공소연"));
-
-        gridPanel.add(new JLabel(" 학번"));
-        gridPanel.add(new JTextField("2020110210"));
-
+        gridPanel.add(new JTextField(""));
         gridPanel.add(new JLabel(" 학과"));
-        gridPanel.add(new JTextField("경제학과"));
-
+        gridPanel.add(new JTextField(""));
+        gridPanel.add(new JLabel(" 학번"));
+        gridPanel.add(new JTextField(""));
+        gridPanel.add(new JLabel(" 이름"));
+        gridPanel.add(new JTextField(""));
         gridPanel.add(new JLabel(" 과목"));
-        gridPanel.add(new JTextField("융합프로그래밍2"));
-
+        gridPanel.add(new JTextField(""));
         gridPanel.add(new JLabel(" 메일"));
-        gridPanel.add(new JTextField("kng001016@gmail.com"));
+        gridPanel.add(new JTextField(""));
 
-        // JTextArea addition
-        JTextArea tf = new JTextArea(4,25);
-        tf.setLineWrap(true);
-        JScrollPane scrollPane = new JScrollPane(tf);
-        scrollPane.setPreferredSize(new Dimension(300, 100));
+        JTextArea textArea = new JTextArea(4, 26); //텍스트 영역 크기(줄, 글자 수)
+        textArea.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
 
-        JPanel textAreaPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
-        textAreaPanel.add(scrollPane);
-        
-        gridPanel.add(textAreaPanel);
-        gridPanel.add(new JLabel());  // Placeholder label for alignment
+        c.add(gridPanel);
+        c.add(scrollPane);
 
-        getContentPane().add(gridPanel);
-
-        setSize(400, 400);
+        setSize(300, 300);
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new G_2020110210_008();
-    }
+	public static void main(String[] args) {
+		new G_2020110210_008();
+	}
 }
